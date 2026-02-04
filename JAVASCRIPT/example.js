507a2d947841fe70b0afd0ab46410d9d7a24ad80
Dhara-06 <dharani2006dgl@gmail.com>
2026-02-04 19:45:57 +0530

    day 1 of JS

---
+++ JAVASCRIPT/example.js
@@ -0,0 +1,8 @@
+var globalScope="i am variable"
+let blockScope="i am let"
+const notChange="i am constant"
+console.log(window.globalScope);
+console.log(window.blockScope);
+console.log(window.notChange);
+
+
